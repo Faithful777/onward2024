709265b2c1ded7056035b287c78f2981bb42511c
Faithful777 <128046605+Faithful777@users.noreply.github.com>
2024-06-21 19:24:34 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -39,7 +39,7 @@ class ImageDataset(Dataset):
         image = self.transform(image)
         label = np.load(label_name)
         print(label.shape)
-        #label = np.expand_dims(label, axis=0)
+        label = np.expand_dims(label, axis=0)
         label = torch.from_numpy(label)
         #label = Image.fromarray(label)
         #print(image.shape)
