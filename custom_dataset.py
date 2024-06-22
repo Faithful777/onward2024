46537051ecb34782194c237d3f87f3fc5326b562
Faithful777 <128046605+Faithful777@users.noreply.github.com>
2024-06-22 20:58:01 +0100

    Update custom_dataset.py

--- custom_dataset.py
+++ custom_dataset.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 from PIL import Image
 import torchvision.transforms as transforms
 import torchvision.transforms.functional as TF
@@ -17,7 +18,7 @@ class MyDataset(Dataset):
 
         # Random crop
         i, j, h, w = transforms.RandomCrop.get_params(
-            image, output_size=(512, 512))
+            image, output_size=(224, 224))
         image = TF.crop(image, i, j, h, w)
         mask = TF.crop(mask, i, j, h, w)
 
@@ -38,7 +39,7 @@ class MyDataset(Dataset):
 
     def __getitem__(self, index):
         image = Image.open(self.image_paths[index])
-        mask = Image.open(self.target_paths[index])
+        mask = label = np.load(self.target_paths[index])
         x, y = self.transform(image, mask)
         return x, y
 
