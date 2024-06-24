ab44bc64214c230e7e706b3264ad386aeaef5379
Faithful777 <128046605+Faithful777@users.noreply.github.com>
2024-06-24 11:57:35 +0100

    Update inference_dataset.py

--- inference_dataset.py
+++ inference_dataset.py
@@ -13,50 +13,34 @@ class InferDataset(Dataset):
         self.target_paths = target_paths
         self.path = path
 
-    def transform(self, image, mask):
+    def transform(self, image):
         # Resize
-        resize = transforms.Resize(size=(230, 230))
+        resize = transforms.Resize(size=(224, 224))
         image = resize(image)
-        mask = resize(mask)
 
-        # Random crop
-        i, j, h, w = transforms.RandomCrop.get_params(
-            image, output_size=(224, 224))
-        image = TF.crop(image, i, j, h, w)
-        mask = TF.crop(mask, i, j, h, w)
-
-        # Random horizontal flipping
-        if random.random() > 0.5:
-            image = TF.hflip(image)
-            mask = TF.hflip(mask)
-
-        # Random vertical flipping
-        if random.random() > 0.5:
-            image = TF.vflip(image)
-            mask = TF.vflip(mask)
 
         # Z-score normalization
         image = TF.normalize(image, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        mask = TF.normalize(mask, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         
         # Transform to tensor
         image = TF.to_tensor(image)
-        #mask = TF.to_tensor(mask)
-        return image, mask
+        return image
 
     def __getitem__(self, index):
         print(self.image_paths[index])
         #print(os.path.join(image_folder, os.path.splitext(self.image_paths[index])[0] + '.npy'))
         image_folder = os.path.join(self.path, 'image')
-        label_folder = os.path.join(self.path, 'label')
+        #label_folder = os.path.join(self.path, 'label')
         image = Image.open(os.path.join(image_folder, self.image_paths[index]))
-        label = np.load(os.path.join(label_folder, os.path.splitext(self.image_paths[index])[0] + '_gt.npy'))
-        print(os.path.join(label_folder, os.path.splitext(self.image_paths[index])[0] + '_gt.npy'))
+        #label = np.load(os.path.join(label_folder, os.path.splitext(self.image_paths[index])[0] + '_gt.npy'))
+        #print(os.path.join(label_folder, os.path.splitext(self.image_paths[index])[0] + '_gt.npy'))
         #label = np.load(os.path.join(label_folder, self.target_paths[index]))
-        label = np.expand_dims(label, axis=0)
-        label = torch.from_numpy(label)
-        x, y = self.transform(image, label)
-        return x, y
+        #label = np.expand_dims(label, axis=0)
+        #label = torch.from_numpy(label)
+        label = os.path.splitext(self.image_paths[index])[0]
+        print(f"label is:{label}")
+        image = self.transform(image)
+        return image, label
 
     def __len__(self):
         return len(self.image_paths)
