3772cea9498d5874bcac19e94cd49bf608bafae7
Faithful777 <128046605+Faithful777@users.noreply.github.com>
2024-06-24 17:57:08 +0100

    Update pretrain_cvmae.py

--- pretrain_cvmae.py
+++ pretrain_cvmae.py
@@ -375,7 +375,7 @@ def pretrain_mae(dataset: str,
         for batch in dataloader:
             views = batch[0]
             #print(f"this is views: {views}")
-            print(f"this is views[0]: {views[0].shape}")
+            #print(f"this is views[0]: {views[0].shape}")
             images = views[0].to(device)  # views contains only a single view
             #_, predictions, targets = model(images)
             #loss = criterion(predictions, targets)
